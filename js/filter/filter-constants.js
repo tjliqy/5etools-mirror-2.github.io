7ca0ce74aa664f6701fc7986863f7e355cdd4bf5
tjliqy <tjliqy@163.com>
2024-08-18 22:19:05 +0800

    fix: 修复筛选页无法确认的问题

--- js/filter/filter-constants.js
+++ js/filter/filter-constants.js
@@ -1,5 +1,5 @@
 export const SUB_HASH_PREFIX_LENGTH = 4;
 
 export const EVNT_VALCHANGE = "valchange";
-export const SOURCE_HEADER = "source";
+export const SOURCE_HEADER = "Source";
 export const TITLE_BTN_RESET = "重置筛选器，按住Shift重置所有选项";
